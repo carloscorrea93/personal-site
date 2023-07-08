5a4749dfb587e296b67040922028e4aaf75f4c65
Carlos Correa <carlosx-34@hotmail.com>
2023-07-07 22:53:17 -0600

    fix: remove donate section and commands

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -43,15 +43,6 @@ export const resume = async (args: string[]): Promise<string> => {
   return 'Opening resume...';
 };
 
-// Donate
-export const donate = async (args: string[]): Promise<string> => {
-  return `thank you for your interest. 
-here are the ways you can support my work:
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
-`;
-};
-
 // Contact
 export const email = async (args: string[]): Promise<string> => {
   window.open(`mailto:${config.email}`);
@@ -109,8 +100,7 @@ directories`;
 };
 
 export const cd = async (args: string[]): Promise<string> => {
-  return `unfortunately, i cannot afford more directories.
-if you want to help, you can type 'donate'.`;
+  return `unfortunately, i cannot afford more directories.`;
 };
 
 export const date = async (args: string[]): Promise<string> => {
@@ -141,14 +131,14 @@ export const sudo = async (args?: string[]): Promise<string> => {
 // Banner
 export const banner = (args?: string[]): string => {
   return `
-█████        ███                       ███████████                                   
-░░███        ░░░                       ░█░░░███░░░█                                   
- ░███        ████  █████ █████  ██████ ░   ░███  ░   ██████  ████████  █████████████  
- ░███       ░░███ ░░███ ░░███  ███░░███    ░███     ███░░███░░███░░███░░███░░███░░███ 
- ░███        ░███  ░███  ░███ ░███████     ░███    ░███████  ░███ ░░░  ░███ ░███ ░███ 
- ░███      █ ░███  ░░███ ███  ░███░░░      ░███    ░███░░░   ░███      ░███ ░███ ░███ 
- ███████████ █████  ░░█████   ░░██████     █████   ░░██████  █████     █████░███ █████
-░░░░░░░░░░░ ░░░░░    ░░░░░     ░░░░░░     ░░░░░     ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ 
+
+   ____           _            ____                           ___ _____ 
+  / ___|__ _ _ __| | ___  ___ / ___|___  _ __ _ __ ___  __ _ / _ \\___ / 
+ | |   / _\` | '__| |/ _ \\/ __| |   / _ \\| '__| '__/ _ \\/ _\` | (_) ||_ \\ 
+ | |__| (_| | |  | | (_) \\__ \\ |__| (_) | |  | | |  __/ (_| |\\__, |__) |
+  \\____\\__,_|_|  |_|\\___/|___/\\____\\___/|_|  |_|  \\___|\\__,_|  /_/____/ 
+                                                                        
+
 
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
