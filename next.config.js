febace20300cfc293d8444b87e9a67490076bee9
Carlos Correa <carlosx-34@hotmail.com>
2024-11-01 11:56:32 -0600

    feat: Add redirect to fb and insta (#3)

--- next.config.js
+++ next.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/contest/4349/fb/ads',
+        destination: 'https://www.facebook.com/share/p/XTJ4EKcmqozrT2uN/?mibextid=WC7FNe',
+        permanent: true,
+      },
+      {
+        source: '/contest/4349/ig/ads',
+        destination: 'https://www.instagram.com/p/DBwcLqHvnAP',
+        permanent: true,
+      },
+    ];
+  },
+};
